65eb74a172572248b2ff3c29a8aca5a855cf48d0
Reddevildragg <Thomas.greener@hotmail.co.uk>
2021-10-29 17:10:18 +0100

    Improved Slider

---
+++ ImprovedSlider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace GreenerGames
+{
+    public class ImprovedSlider : Slider, IEndDragHandler
+    {
+        public UnityAction<float> onDragEnd;
+        
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            onDragEnd?.Invoke(value);
+        }
+    }
+}
